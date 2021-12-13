4e29038fe87e42cc4547949734ec3d411381e608
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-12-13 09:20:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "Hello World!\n"
+  return "Hello World!\n, Hello John!\n")
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
